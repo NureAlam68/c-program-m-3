7d63b132f103816a1d547f2685547dc3e9e3c504
NureAlam68 <nurealam151068@gmail.com>
2025-04-10 16:10:12 +0600

    more for loop

--- for-loop.c
+++ for-loop.c
@@ -1,7 +1,60 @@
+// #include <stdio.h>
+// int main() {
+//     for(int i = 1; i <= 3; i++) {
+//         printf("Hello World %d\n", i);
+//     }
+//     return 0;
+// }
+
+// #include <stdio.h>
+// int main() {
+//     for(int i = 1; i <= 100; i++) {
+//         printf("%d\n", i);
+//     }
+//     return 0;
+// }
+
+// odd numbers
+// #include <stdio.h>
+// int main() {
+//     for(int i = 1; i <= 10; i = i + 2) {
+//         printf("%d\n", i);
+//     }
+//     return 0;
+// }
+
+// even numbers
+// #include <stdio.h>
+// int main() {
+//     for(int i = 2; i <= 10; i = i + 2) {
+//         printf("%d\n", i);
+//     }
+//     return 0;
+// }
+
+// 4 er multiples
+// #include <stdio.h>
+// int main() {
+//     for(int i = 4; i <= 100; i = i + 4) {
+//         printf("%d\n", i);
+//     }       
+//     return 0;
+// }
+
+// 100 to 1
+// #include <stdio.h>
+// int main() {
+//     for(int i = 100; i >= 1; i--) {
+//         printf("%d\n", i);
+//     }
+//     return 0;
+// }
+
+// 2 multiples from 1 to 100
 #include <stdio.h>
 int main() {
-    for(int i = 1; i <= 3; i++) {
-        printf("Hello World %d\n", i);
+    for(int i = 2; i <= 64; i = i * 2) {
+        printf("%d\n", i);
     }
     return 0;
 }

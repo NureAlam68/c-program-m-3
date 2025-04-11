5fb76e64ae5e5eceabb69446b893da7824b56086
NureAlam68 <nurealam151068@gmail.com>
2025-04-12 01:36:45 +0600

    sum 1 to n

---
+++ sum.c
@@ -0,0 +1,14 @@
+// n th number sum
+#include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int sum = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        sum = sum + i;
+    }
+    printf("%d\n", sum);
+    return 0;
+}

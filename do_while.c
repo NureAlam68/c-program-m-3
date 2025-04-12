a1963d99abd0acbb114754c79b2de07d414bd568
NureAlam68 <nurealam151068@gmail.com>
2025-04-12 12:55:12 +0600

    do while loop

---
+++ do_while.c
@@ -0,0 +1,12 @@
+// print 1 to 10 with do while loop
+#include <stdio.h>
+int main()
+{
+    int i = 1;
+    do
+    {
+        printf("%d\n", i);
+        i++;
+    } while (i <= 10);
+    return 0;
+}
